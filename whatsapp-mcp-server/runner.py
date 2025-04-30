23070fa714df1e28f2f5003c0e696a4a7b52042d
Bhumika Makwana <38176502+clueless28@users.noreply.github.com>
2025-04-30 19:18:24 +0530

    Update runner.py

--- whatsapp-mcp-server/runner.py
+++ whatsapp-mcp-server/runner.py
@@ -4,6 +4,13 @@ from fastapi import FastAPI, Request
 import importlib.util
 import os
 from pathlib import Path
+from flask import Flask, Response
+import time
+import json
+from flask import jsonify
+from mcp.server.fastmcp import FastMCP
+from flask import Flask, render_template
+
 
 def load_send_message(path_to_main_py):
     # Construct a module spec
@@ -14,38 +21,13 @@ def load_send_message(path_to_main_py):
     spec.loader.exec_module(module)
     return module.send_message
 
-
-from flask import Flask, Response
-import time
-import json
-from flask import jsonify
-from mcp.server.fastmcp import FastMCP
-from flask import Flask, render_template
 app = Flask(__name__)
 
 # Configure Gemini
-genai.configure(api_key="AIzaSyD8bwyn4220ZKL9biFi3_46tRlgKrZETbg")
+genai.configure(api_key= os.getenv("GENAI_API_KEY"))
 
 
 send_message = load_send_message(os.path.join(os.getcwd(), "main.py"))
-
-# def send_whatsapp_message(recipient: str, message: str):
-#     """Directly execute MCP tool using subprocess"""
-#     try:
-#         print("sending.....")
-#         # subprocess.run([
-#         #     "python", 
-#         #     "/Users/bhumikamakwana/Desktop/clost_web/whatsapp-mcp/whatsapp-mcp-server/main.py",
-#         #     "send_message",
-#         #     "--recipient",
-#         #     recipient,
-#         #     "--message",
-#         #     message
-#         # ], check=True)
-#         pass
-#     except subprocess.CalledProcessError as e:
-#         print(f"Failed to send message: {str(e)}")
-#         raise
 
 def generate_gemini_response(prompt: str) -> str:
     """Generate response using Gemini 1.5 Flash model"""
@@ -59,29 +41,6 @@ def generate_gemini_response(prompt: str) -> str:
     )
     return response.text
 
-# @app.post("/incoming")
-# async def handle_message(request: Request):
-#     data = await request.json()
-#     message = data.get("text")
-#     sender = data.get("from")
-
-#     if not message or not sender:
-#         return {"error": "Missing message or sender"}
-
-#     try:
-#         # Generate response with Gemini
-#         gemini_response = generate_gemini_response(message)
-#         print(gemini_response)
-        
-#         # Send response directly using MCP tool
-#         #send_whatsapp_message(recipient=sender, message=gemini_response)
-#         res = send_message(sender, gemini_response)
-        
-#         return {"status": "Message processed successfully"}
-        
-#     except Exception as e:
-#         return {"error": str(e)}
-    
 from flask import Flask, request, jsonify
 @app.post("/incoming")
 def handle_message():
